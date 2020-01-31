9132ae23b30aa48bfb7c2ae6de9d360f0fe02577
Hasan <ms.abulhasan@hotmail.com>
2020-01-31 17:50:10 +0800

    Database init file in docket initialze directory

---
+++ docker-entrypoint-initdb.d/init.sql
@@ -0,0 +1,42 @@
+CREATE USER docker;
+CREATE DATABASE docker;
+GRANT ALL PRIVILEGES ON DATABASE docker TO docker;
+-- Sales Transaction table that will store sales information
+CREATE TABLE public.sales(
+   sales_id serial PRIMARY KEY,
+   customer_id VARCHAR (50)  NOT NULL,
+   salesperson_id VARCHAR (50) UNIQUE NOT NULL,
+   card_model_id integer NOT NULL,
+   iscardsold varchar(3) NOT NULL,
+   created_on TIMESTAMP NOT NULL,
+   FOREIGN KEY (salesperson_id,card_model_id)
+);
+-- Customer Table it will store customer basic info
+CREATE TABLE public.customer(
+   customer_id serial PRIMARY KEY,
+   customer_name VARCHAR (50)  NOT NULL,
+   customer_phone VARCHAR (50) NOT NULL,
+   created_on TIMESTAMP NOT NULL
+);
+-- Sales Man Table it will store  sales person info
+CREATE TABLE public.salesperson(
+	salesperson_id serial PRIMARY KEY,
+	salesperson_name VARCHAR(50) NOT NULL
+);
+-- Manufacturer table 
+CREATE TABLE public.manufacturer(
+	manufacturer_id serial PRIMARY KEY,
+	manufacturer_name VARCHAR(50) NOT NULL
+);
+-- Car table - contains cars info
+CREATE TABLE public.car(
+	car_model_id serial PRIMARY KEY,
+	car_manufacturer_id integer NOT NULL,
+	car_model_name VARCHAR(50) NOT NULL,
+	car_model_varient VARCHAR(50) NOT NULL,
+	car_model_number VARCHAR(50) NOT NULL,
+	weight NUMERIC (5, 2) NOT NULL,
+	engine_capacity NUMERIC(5,3) NOT NULL,
+	price NUMERIC(5,3) NOT NULL,
+   FOREIGN KEY (car_manufacturer_id)
+);
